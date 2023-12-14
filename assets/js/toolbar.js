397131c464e6fa4b336f0f3decbf5673e2fa8504
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-12-14 09:57:34 +0100

    Change button on clompleted to "close" button instead of "cancel".

--- assets/js/toolbar.js
+++ assets/js/toolbar.js
@@ -37,6 +37,32 @@ class FormNavigationManager {
       }
     })
 
+    /* Form complete - Hide back button */ 
+    formsApi.onSubmitted(() => {
+      
+      // Hide back button
+      const backButton = document.getElementById("back-button");
+      if(backButton) {
+        backButton.classList.add("u-display--none");
+        backButton.setAttribute('aria-hidden', "true");
+      }
+
+      //Hide cancel button
+      const cancelButton = document.getElementById("cancel-button");
+      if (cancelButton) {
+        cancelButton.classList.add("u-display--none");
+        cancelButton.setAttribute('aria-hidden', "true");
+      }
+
+      //Show close button
+      const closeButton = document.getElementById("close-button");
+      if (closeButton) {
+        closeButton.classList.remove("u-display--none"); 
+        closeButton.removeAttribute('aria-hidden'); 
+      }
+
+    })
+
     /* Scroll up on step change */
     formsApi.onStepChange((from,to) => {
       window.scrollTo(0, 0);
@@ -53,6 +79,18 @@ class FormNavigationManager {
           } else {
             parent.location.reload();
           }
+        }
+      });
+    }
+
+    /* Close button */ 
+    const closeButton = document.getElementById("close-button");
+    if (closeButton) {
+      closeButton.addEventListener("click", function () {
+        if(opener) {
+          opener.location.reload();
+        } else {
+          parent.location.reload();
         }
       });
     }
