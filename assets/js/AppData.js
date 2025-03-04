44e5f9b11b434fc0eb4bbda718d439525cb57f36
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-03-04 13:42:45 +0100

    feat: add ability to change forms by param

--- assets/js/AppData.js
+++ assets/js/AppData.js
@@ -1,3 +1,7 @@
+const urlParams = new URLSearchParams(window.location.search);
+const formIdWebParam = urlParams.get("formIdWeb");
+const formIdAppParam = urlParams.get("formIdApp");
+
 const AppData = {
   urls: {
     ios: "https://apps.apple.com/se/app/ett-b%C3%A4ttre-helsingborg/id1061079182",
@@ -5,8 +9,8 @@ const AppData = {
     noLink: "#nolink",
   },
   formIds: {
-    app: 'vEKRiB3le1BSsgo18HW2',
-    web: 'U3X0lhXaQ4B2dBs2rk5K',
+    app: formIdAppParam || "vEKRiB3le1BSsgo18HW2",
+    web: formIdWebParam || "U3X0lhXaQ4B2dBs2rk5K",
   },
 };
 
